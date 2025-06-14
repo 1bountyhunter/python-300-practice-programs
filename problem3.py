c23152bca68c6a07289cd6cd1822744269002961
1bountyhunter <mahak280303@gmail.com>
2025-06-14 21:21:59 +0530

    name and age oof user

---
+++ problem3.py
@@ -0,0 +1,6 @@
+# WAP to enter name and age and display it on console
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello, " + name + "! You are " + str(age) + " years old.")
+print(f"My name is {name} and I am {age} years old.")
